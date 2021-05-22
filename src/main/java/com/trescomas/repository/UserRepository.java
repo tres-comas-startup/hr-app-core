0807fb0d6c568bcf4d9575a64a10c7e797b733d0
Gleb Remniov <g.remniov@gmail.com>
2021-05-22 21:09:22 +0300

    Minor bugfix

--- src/main/java/com/trescomas/repository/UserRepository.java
+++ src/main/java/com/trescomas/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package com.trescomas.repository;
 
 import com.trescomas.domain.model.User;
-import com.trescomas.exception.ResourceNotFoundException;
 import com.trescomas.domain.projection.user.UserExcerptProjection;
+import com.trescomas.exception.ResourceNotFoundException;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import javax.validation.constraints.NotNull;
 import java.util.Optional;
@@ -19,6 +20,7 @@ public interface UserRepository extends BaseRepository<User, Long> {
 
     Optional<User> findByUsername(@NotNull String username);
 
+    @RestResource(exported = false)
     @Query("from User u join fetch u.roles where u.username = ?1")
     Optional<User> findByUsernameEager(String username);
 
